d68c9b8020eec040cbeba216697e8f04799b9bf3
vromanenko <vromanenko@artesia-solutions.com>
2025-06-02 23:36:25 +0400

    try mirror

--- internal/ytdl/ytdl.go
+++ internal/ytdl/ytdl.go
@@ -3,6 +3,7 @@ package ytdl
 import (
 	"context"
 	_ "embed"
+	"fmt"
 	"net"
 	"net/http"
 	"net/url"
@@ -88,23 +89,25 @@ func NewYoutubeDL() *YoutubeDL {
 }
 
 func (dl *YoutubeDL) GetVideoInfo(ctx context.Context, videoID string) (*youtube.Video, string, error) {
-	video, err := dl.client.GetVideoContext(ctx, videoID)
-	if err != nil {
-		return nil, "", err
-	}
-	formats := video.Formats.Select(func(format youtube.Format) bool {
-		if strings.HasPrefix(format.MimeType, "video/") {
-			return true
-		}
-		return false
-	})
-	if len(formats) == 0 {
-		return nil, "", youtube.ErrNoFormat
-	}
-	format := formats[0]
-	u, err := dl.client.GetStreamURLContext(ctx, video, &format)
-	if err != nil {
-		return nil, "", err
-	}
-	return video, u, nil
+	return &youtube.Video{}, fmt.Sprintf("https://invidious.f5.si/latest_version?id=%s&itag=18", videoID), nil
+	// video, err := dl.client.GetVideoContext(ctx,
+	// 	fmt.Sprintf("https://www.youtube.com/shorts/%s", videoID))
+	// if err != nil {
+	// 	return nil, "", err
+	// }
+	// formats := video.Formats.Select(func(format youtube.Format) bool {
+	// 	if strings.HasPrefix(format.MimeType, "video/") {
+	// 		return true
+	// 	}
+	// 	return false
+	// })
+	// if len(formats) == 0 {
+	// 	return nil, "", youtube.ErrNoFormat
+	// }
+	// format := formats[0]
+	// u, err := dl.client.GetStreamURLContext(ctx, video, &format)
+	// if err != nil {
+	// 	return nil, "", err
+	// }
+	// return video, u, nil
 }
